16ef5bb7dd337816d5ce53acc1dabe08f49f909e
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-10-15 21:29:05 -0700

    Add item query and mutation

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,14 @@
+// Use forwardTo if query is identical to Prisma method
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  dogs(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    return global.dogs;
-  }
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+
+  //   return items;
+  // }
+
+  items: forwardTo('db')
 };
 
 module.exports = Query;
