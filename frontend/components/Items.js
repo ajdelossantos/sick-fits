8c83ab0c0a2a9ca3c1212e70f67d0ca5e816816f
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-10-23 23:18:03 -0700

    Debug prisma.yaml; implement Items and Items components and mutations

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Item from './Item';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -33,7 +34,7 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <p>Items!</p>
+        <h2>Items!</h2>
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
@@ -48,7 +49,7 @@ class Items extends Component {
             return (
               <ItemsList>
                 {data.items.map(item => (
-                  <p>{item.title}</p>
+                  <Item key={item.id} item={item} />
                 ))}
               </ItemsList>
             );
